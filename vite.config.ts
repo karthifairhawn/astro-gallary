b5621431a059083128e47ed2c1cb45547966f24d
Karthi Fair Hawn <karthifairhawn@gmail.com>
2025-02-10 01:41:43 +0530

    changed config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/astro-gallery/', // for GitHub Pages
+  base: '/', // for GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
